425b4f2e5ace05d30d262a40b1f264a2c119eed7
Wim Stalmans <wim.stalmans@gmail.com>
2024-05-15 11:00:28 +0200

    smart contract writing

--- back/hardhat.config.js
+++ back/hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.24",
+    solidity: "0.8.17",
     networks: {
         sepolia: {
             url: process.env.RPC_URL,
