bd6ce6231b62083b7680d3cac9ff4598fbab5872
Wim Stalmans <wim.stalmans@gmail.com>
2024-05-15 11:15:34 +0200

    deploymentscript

---
+++ back/ignition/modules/EscrowDeploy.js
@@ -0,0 +1,14 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const EscrowModule = buildModule("EscrowModule", (m) => {
+  
+  // Provide the constructor arguments for the Escrow contract
+  const arbiterAddress = "0xeb5925d4Cb344Ab27b82679CeDf3FDa8a45E2e1F"; // Replace with the actual arbiter address
+  const beneficiaryAddress = "0x16C8Df447537A0a15a87d439d9d7eA8790f563a4"; // Replace with the actual beneficiary address
+  
+  const escrow = m.contract("Escrow");
+
+  return { escrow };
+});
+
+module.exports = EscrowModule;
