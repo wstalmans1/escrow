7024dc7e1391d6f805a7a013e1fd5e86c7eccb0e
Wim Stalmans <wim.stalmans@gmail.com>
2024-05-16 14:27:06 +0200

    app

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
-
-
+import { ethers } from 'ethers';
 
 function App() {
+  const [balance, setBalance] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const init = async () => {
+      if (window.ethereum == null) {
+        console.log("MetaMask not installed; using read-only defaults");
+        setProvider(ethers.getDefaultProvider());
+      } else {
+        const newProvider = new ethers.BrowserProvider(window.ethereum);
+        setProvider(newProvider);
+        const newSigner = await newProvider.getSigner();
+        setSigner(newSigner);
+        //const newAccount = await newSigner.getAddress();
+        const newAccount = "0x0693B05E5C8aeF4DA37c2Bc0E7B507e602404348";
+        setAccount(newAccount);
+      }
+    };
+    init();
+  }, []);
+
+  const fetchBalance = async () => {
+    if (signer && account) {
+      const balance = await provider.getBalance(account);
+      setBalance(ethers.formatEther(balance));
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and ethers
-        </a>
+      <header className="App-header">        
+        {account && <p>Account: {account}</p>}
+        <button onClick={fetchBalance}>Get Balance</button>
+        {balance !== null && <p>Balance: {balance} ETH</p>}
       </header>
     </div>
   );
